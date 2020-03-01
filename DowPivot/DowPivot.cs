63d8f85b4549d44abd76b0cab7f56a649c65bce9
Jean Alves <jeanc.alves@hotmail.com>
2020-03-01 15:39:38 -0300

    Alterado namespace da classe DowPivot

--- DowPivot/DowPivot.cs
+++ DowPivot/DowPivot.cs
@@ -1,28 +1,5 @@
-#region Using declarations
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Xml.Serialization;
-using NinjaTrader.Cbi;
-using NinjaTrader.Gui;
-using NinjaTrader.Gui.Chart;
-using NinjaTrader.Gui.SuperDom;
-using NinjaTrader.Gui.Tools;
-using NinjaTrader.Data;
-using NinjaTrader.NinjaScript;
-using NinjaTrader.Core.FloatingPoint;
-using NinjaTrader.NinjaScript.DrawingTools;
-#endregion
-
 //This namespace holds Indicators in this folder and is required. Do not change it. 
-namespace NinjaTrader.NinjaScript.Indicators.JiraiyaIndicators.DowPivot
+namespace NinjaTrader.NinjaScript.Indicators.JiraiyaIndicators
 {
 	public class DowPivot : Indicator
 	{
@@ -40,8 +17,6 @@ namespace NinjaTrader.NinjaScript.Indicators.JiraiyaIndicators.DowPivot
 				DrawVerticalGridLines						= false;
 				PaintPriceMarkers							= true;
 				ScaleJustification							= NinjaTrader.Gui.Chart.ScaleJustification.Right;
-				//Disable this property if your indicator requires custom values that cumulate with each new market data event. 
-				//See Help Guide for additional information.
 				IsSuspendedWhileInactive					= true;
 			}
 			else if (State == State.Configure)
@@ -62,19 +37,19 @@ namespace NinjaTrader.NinjaScript.Indicators
 {
 	public partial class Indicator : NinjaTrader.Gui.NinjaScript.IndicatorRenderBase
 	{
-		private JiraiyaIndicators.DowPivot.DowPivot[] cacheDowPivot;
-		public JiraiyaIndicators.DowPivot.DowPivot DowPivot()
+		private JiraiyaIndicators.DowPivot[] cacheDowPivot;
+		public JiraiyaIndicators.DowPivot DowPivot()
 		{
 			return DowPivot(Input);
 		}
 
-		public JiraiyaIndicators.DowPivot.DowPivot DowPivot(ISeries<double> input)
+		public JiraiyaIndicators.DowPivot DowPivot(ISeries<double> input)
 		{
 			if (cacheDowPivot != null)
 				for (int idx = 0; idx < cacheDowPivot.Length; idx++)
 					if (cacheDowPivot[idx] != null &&  cacheDowPivot[idx].EqualsInput(input))
 						return cacheDowPivot[idx];
-			return CacheIndicator<JiraiyaIndicators.DowPivot.DowPivot>(new JiraiyaIndicators.DowPivot.DowPivot(), input, ref cacheDowPivot);
+			return CacheIndicator<JiraiyaIndicators.DowPivot>(new JiraiyaIndicators.DowPivot(), input, ref cacheDowPivot);
 		}
 	}
 }
@@ -83,12 +58,12 @@ namespace NinjaTrader.NinjaScript.MarketAnalyzerColumns
 {
 	public partial class MarketAnalyzerColumn : MarketAnalyzerColumnBase
 	{
-		public Indicators.JiraiyaIndicators.DowPivot.DowPivot DowPivot()
+		public Indicators.JiraiyaIndicators.DowPivot DowPivot()
 		{
 			return indicator.DowPivot(Input);
 		}
 
-		public Indicators.JiraiyaIndicators.DowPivot.DowPivot DowPivot(ISeries<double> input )
+		public Indicators.JiraiyaIndicators.DowPivot DowPivot(ISeries<double> input )
 		{
 			return indicator.DowPivot(input);
 		}
@@ -99,12 +74,12 @@ namespace NinjaTrader.NinjaScript.Strategies
 {
 	public partial class Strategy : NinjaTrader.Gui.NinjaScript.StrategyRenderBase
 	{
-		public Indicators.JiraiyaIndicators.DowPivot.DowPivot DowPivot()
+		public Indicators.JiraiyaIndicators.DowPivot DowPivot()
 		{
 			return indicator.DowPivot(Input);
 		}
 
-		public Indicators.JiraiyaIndicators.DowPivot.DowPivot DowPivot(ISeries<double> input )
+		public Indicators.JiraiyaIndicators.DowPivot DowPivot(ISeries<double> input )
 		{
 			return indicator.DowPivot(input);
 		}
