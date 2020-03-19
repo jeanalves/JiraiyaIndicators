c3ac6d865d0c6ad7717408aebf1e25afd27326f0
Jean Alves <jeanc.alves@hotmail.com>
2020-03-19 03:02:42 -0300

    Iniciada codificação de calculo do indicador DowPivot

--- Core/DowPivot/Calculation.cs
+++ Core/DowPivot/Calculation.cs
@@ -1,6 +1,37 @@
+using NinjaTrader.NinjaScript;
+using System.Collections.Generic;
+
 namespace NinjaTrader.Custom.Indicators.JiraiyaIndicators.DowPivot
 {
     public class Calculation
     {
+        // Fields
+
+        protected List<Pivot> pivotList = new List<Pivot>();
+
+        protected readonly NinjaScriptBase owner;
+        protected readonly LogPrinter logPrinter;
+
+        // Initialization
+
+        protected Calculation(NinjaScriptBase owner, LogPrinter logPrinter)
+        {
+            this.owner = owner;
+            this.logPrinter = logPrinter;
+        }
+
+        public void Calculate()
+        {
+
+        }
+
+        // Miscellaneous
+        
+        private enum CalculationStage
+        {
+            FirstPoint,
+            EachBarSwingPoint,
+            EachTickSwingPoint
+        }
     }
 }
---
+++ Core/DowPivot/PivotCalculation.cs
@@ -0,0 +1,9 @@
+﻿using NinjaTrader.NinjaScript;
+
+namespace NinjaTrader.Custom.Indicators.JiraiyaIndicators.DowPivot
+{
+    public class PivotCalculation : Calculation
+    {
+        public PivotCalculation(NinjaScriptBase owner, LogPrinter logPrinter) : base(owner, logPrinter) { }
+    }
+}
