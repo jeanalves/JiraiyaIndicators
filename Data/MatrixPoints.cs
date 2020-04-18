c844f2ad106a6fdc8558e6def9321d71532d19d1
Jean Alves <jeanc.alves@hotmail.com>
2020-04-18 16:41:55 -0300

    Criada classe "MatrixPoints"

---
+++ Data/MatrixPoints.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace NinjaTrader.Custom.Indicators.JiraiyaIndicators
+{
+    public class MatrixPoints
+    {
+        private readonly List<Point> pointsList = new List<Point>();
+
+        public MatrixPoints() { }
+
+        public MatrixPoints(List<Point> pointsList)
+        {
+            this.pointsList = pointsList;
+        }
+
+        public void AddPoint(Point point)
+        {
+            PointList.Add(point);
+        }
+
+        public List<Point> PointList
+        {
+            get
+            {
+                return pointsList;
+            }
+        }
+    }
+}
