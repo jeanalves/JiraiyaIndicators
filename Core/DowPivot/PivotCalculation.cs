62abc8588477178521d610b307862881dc781381
Jean Alves <jeanc.alves@hotmail.com>
2020-04-27 22:41:48 -0300

    Construção da classe PivotCalculation

--- Core/DowPivot/PivotCalculation.cs
+++ Core/DowPivot/PivotCalculation.cs
@@ -1,4 +1,5 @@
-﻿using NinjaTrader.NinjaScript;
+using NinjaTrader.Custom.Indicators.JiraiyaIndicators.PriceActionSwing;
+using NinjaTrader.NinjaScript;
 
 namespace NinjaTrader.Custom.Indicators.JiraiyaIndicators.DowPivot
 {
@@ -6,9 +7,33 @@ namespace NinjaTrader.Custom.Indicators.JiraiyaIndicators.DowPivot
     {
         public PivotCalculation(NinjaScriptBase owner) : base(owner) { }
 
-        protected override CalculationData OnNewUpdateEvent()
+        protected override CalculationData OnNewUpdateEvent(PriceActionSwingClass priceActionSwingClass)
         {
-            throw new System.NotImplementedException();
+            MatrixPoints matrixPoints = new MatrixPoints();
+            bool isNewMatrixPoints = true;
+
+            if(priceActionSwingClass.GetPoint(3) == null)
+            {
+                return new CalculationData(false);
+            }
+
+            matrixPoints.AddPoint(priceActionSwingClass.GetPoint(0));
+            matrixPoints.AddPoint(priceActionSwingClass.GetPoint(1));
+            matrixPoints.AddPoint(priceActionSwingClass.GetPoint(2));
+            matrixPoints.AddPoint(priceActionSwingClass.GetPoint(3));
+
+            if (matrixPoints.PointsList[3].CurrentSideSwing == Point.SidePoint.Low)
+            {
+                isNewMatrixPoints = matrixPoints.PointsList[3].Price > matrixPoints.PointsList[1].Price &&
+                                    matrixPoints.PointsList[2].Price > matrixPoints.PointsList[0].Price;
+            }
+            else if(matrixPoints.PointsList[3].CurrentSideSwing == Point.SidePoint.High)
+            {
+                isNewMatrixPoints = matrixPoints.PointsList[3].Price < matrixPoints.PointsList[1].Price &&
+                                    matrixPoints.PointsList[2].Price < matrixPoints.PointsList[0].Price;
+            }
+
+            return isNewMatrixPoints == true ? new CalculationData(true, matrixPoints) : new CalculationData(false);
         }
     }
 }
