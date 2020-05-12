85806260c8af596d19a5e42e6b23a9968e36fe9f
Jean Alves <jeanc.alves@hotmail.com>
2020-05-12 00:59:13 -0300

    Criado código para evitar adicionar objetos repetidos na lista "matrixPointsList""

--- Core/DowTheory/Calculation.cs
+++ Core/DowTheory/Calculation.cs
@@ -45,32 +45,50 @@ namespace NinjaTrader.Custom.Indicators.JiraiyaIndicators.DowPivot
 
         private void AddOrUpdateIfNewMatrixPoints(CalculationData calculationData)
         {
-            switch(calculationData.currentMatrixPoints.trendSideSignal)
+            // Here, no code is needed to update the matrix due to the object
+            // reference already passed in the list of points, from MatrixPoints.PointsList
+            if (!IsNewMatrixTheSameTheLastOne(calculationData.currentMatrixPoints))
             {
-                // Add long pattern if the first point was an low
-                case MatrixPoints.WhichTrendSideSignal.Bullish:
-                    matrixPointsList.Add(new MatrixPoints(calculationData.currentMatrixPoints.PointsList,
-                                                          matrixPointsList.Count,
-                                                          MatrixPoints.WhichTrendSideSignal.Bullish,
-                                                          calculationData.currentMatrixPoints.graphicPatternType));
-                    break;
+                switch (calculationData.currentMatrixPoints.trendSideSignal)
+                {
+                    // Add long pattern if the first point was an low
+                    case MatrixPoints.WhichTrendSideSignal.Bullish:
+                        matrixPointsList.Add(new MatrixPoints(calculationData.currentMatrixPoints.PointsList,
+                                                              matrixPointsList.Count,
+                                                              MatrixPoints.WhichTrendSideSignal.Bullish,
+                                                              calculationData.currentMatrixPoints.graphicPatternType));
+                        break;
 
-                // Add short pattern if the first point was an high
-                case MatrixPoints.WhichTrendSideSignal.Bearish:
-                    matrixPointsList.Add(new MatrixPoints(calculationData.currentMatrixPoints.PointsList,
-                                                          matrixPointsList.Count,
-                                                          MatrixPoints.WhichTrendSideSignal.Bearish,
-                                                          calculationData.currentMatrixPoints.graphicPatternType));
-                    break;
+                    // Add short pattern if the first point was an high
+                    case MatrixPoints.WhichTrendSideSignal.Bearish:
+                        matrixPointsList.Add(new MatrixPoints(calculationData.currentMatrixPoints.PointsList,
+                                                              matrixPointsList.Count,
+                                                              MatrixPoints.WhichTrendSideSignal.Bearish,
+                                                              calculationData.currentMatrixPoints.graphicPatternType));
+                        break;
+                }
+            }
+        }
+
+        private bool IsNewMatrixTheSameTheLastOne(MatrixPoints newMatrixPoints)
+        {
+            if (matrixPointsList.Count <= 0)
+            {
+                return false;
+            }
+            
+            MatrixPoints lastMatrix = matrixPointsList[matrixPointsList.Count - 1];
+
+            // Test all points except the last one
+            for (int i = 0; i<= newMatrixPoints.PointsList.Count - 2; i++)
+            {
+                if (lastMatrix.PointsList[i].Index != newMatrixPoints.PointsList[i].Index)
+                {
+                    return false;
+                }
             }
 
-            // Update long pivot
-            if (calculationData.currentMatrixPoints.PointsList[3].CurrentSideSwing == Point.SidePoint.Low &&
-                calculationData.currentMatrixPoints.PointsList[0].Price > matrixPointsList[matrixPointsList.Count -1].PointsList[0].Price)
-            {
-                //matrixPointsList[matrixPointsList.Count - 1] = calculationData.currentMatrixPoint;
-            }
-
+            return true;
         }
 
         // Properties
