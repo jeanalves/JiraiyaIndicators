e4667b0732e919c3470f075bf48659c7a4dbeeb5
Jean Alves <jeanc.alves@hotmail.com>
2020-05-30 14:14:15 -0300

    Atualizada lógica de uso dos ultimos 4 pontos no calculo de pivos
    Foi alterada a ordem em que se interage com os últimos 4 pontos calculados, a nova lógica consiste em tratar todos os pontos sempre com um índice ao contrário, o ultimo ponto será o índice 0 e o penúltimo será o índice 1 e assim por diante.

--- Core/DowTheory/PivotCalculation.cs
+++ Core/DowTheory/PivotCalculation.cs
@@ -24,34 +24,34 @@ namespace NinjaTrader.Custom.Indicators.JiraiyaIndicators.DowPivot
             ResetWhichTrendVariableBySMA(20);
 
             //----Bearish----|---Bullish---
-            //----0----------|----------3--
-            //-----\---2-----|-----1---/---
+            //----3----------|----------0--
+            //-----\---1-----|-----2---/---
             //------\-/-\----|----/-\-/----
-            //-------1---\---|---/---2-----
-            //------------3--|--0----------
+            //-------2---\---|---/---1-----
+            //------------0--|--3----------
 
-            pointsList.Add(priceActionSwingClass.GetPoint(3)); // First point or  pointsList[0]
-            pointsList.Add(priceActionSwingClass.GetPoint(2)); // Second point or pointsList[1]
-            pointsList.Add(priceActionSwingClass.GetPoint(1)); // Third point or  pointsList[2]
-            pointsList.Add(priceActionSwingClass.GetPoint(0)); // Fourth point or pointsList[3]
+            pointsList.Add(priceActionSwingClass.GetPoint(0)); // Last point or   pointsList[0]
+            pointsList.Add(priceActionSwingClass.GetPoint(1)); // 1 point ago or  pointsList[1]
+            pointsList.Add(priceActionSwingClass.GetPoint(2)); // 2 points ago or pointsList[2]
+            pointsList.Add(priceActionSwingClass.GetPoint(3)); // 3 points ago or pointsList[3]
 
             // Test a long pivot
-            if ((pointsList[0].CurrentSideSwing == Point.SidePoint.Low && whichTrend != MatrixPoints.WhichTrendSideSignal.Bullish) ||
-                (pointsList[0].CurrentSideSwing == Point.SidePoint.Low && IsNewMatrixTheSameTheLastOne(pointsList)))
+            if ((pointsList[3].CurrentSideSwing == Point.SidePoint.Low && whichTrend != MatrixPoints.WhichTrendSideSignal.Bullish) ||
+                (pointsList[3].CurrentSideSwing == Point.SidePoint.Low && IsNewMatrixTheSameTheLastOne(pointsList)))
             {
-                isNewMatrixPoints = pointsList[0].Price < pointsList[2].Price &&
-                                    pointsList[1].Price < pointsList[3].Price;
+                isNewMatrixPoints = pointsList[3].Price < pointsList[1].Price &&
+                                    pointsList[2].Price < pointsList[0].Price;
                 if (isNewMatrixPoints)
                 {
                     whichTrend = MatrixPoints.WhichTrendSideSignal.Bullish;
                 }
             }
             // Test a short pivot
-            else if ((pointsList[0].CurrentSideSwing == Point.SidePoint.High && whichTrend != MatrixPoints.WhichTrendSideSignal.Bearish) ||
-                     (pointsList[0].CurrentSideSwing == Point.SidePoint.High && IsNewMatrixTheSameTheLastOne(pointsList)))
+            else if ((pointsList[3].CurrentSideSwing == Point.SidePoint.High && whichTrend != MatrixPoints.WhichTrendSideSignal.Bearish) ||
+                     (pointsList[3].CurrentSideSwing == Point.SidePoint.High && IsNewMatrixTheSameTheLastOne(pointsList)))
             {
-                isNewMatrixPoints = pointsList[0].Price > pointsList[2].Price &&
-                                    pointsList[1].Price > pointsList[3].Price;
+                isNewMatrixPoints = pointsList[3].Price > pointsList[1].Price &&
+                                    pointsList[2].Price > pointsList[0].Price;
                 if (isNewMatrixPoints)
                 {
                     whichTrend = MatrixPoints.WhichTrendSideSignal.Bearish;
